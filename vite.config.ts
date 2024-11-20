b83c01bd17914c77f4733fcc702a26ed4b68fac5
Roman Turchin <roman@turch.in>
2024-11-20 17:49:02 -0500

    fix base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
   build: {
     outDir: './dist'
   },
-  base: '/vite-boilerplate/'
+  base: '/twa-plo/'
 });
